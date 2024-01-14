576f0752135673df9341c9f28ac0c38f87548e38
Вячеслав Тю <vyacheslavtyu@Vaceslavs-MacBook-Pro.local>
2024-01-14 20:13:16 +0500

    fixed

---
+++ src/pages/catalog/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import css from './index.module.css'
+import {useTranslation} from "next-i18next";
+import Head from "next/head";
+import Breadcrumbs from "@/components/shared/breadcrumbs/breadcrumbs";
+import CatalogList from "@/components/pages/catalog/catalog-list/catalog-list";
+import {GetStaticProps} from "next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+
+interface props {
+
+}
+
+const Index = (props: props) => {
+    const {t} = useTranslation()
+    return (
+        <>
+            <Head>
+                <title>
+                    {t('header.catalog')}
+                </title>
+            </Head>
+            <section className={css.catalog}>
+                <div className={'container'}>
+                    <Breadcrumbs items={[
+                        {
+                            path: '/',
+                            label: t('header.home')
+                        },
+                        {
+                            path: '/catalog',
+                            label: t('header.catalog')
+                        }
+                    ]}/>
+                    <h1 className={css.title}>
+                        {t('header.catalog')} {t('products.plural').toLowerCase()}
+                    </h1>
+                    <CatalogList/>
+                </div>
+            </section>
+        </>
+    );
+};
+
+type Props = {};
+export const getStaticProps: GetStaticProps<Props> = async ({locale}) => {
+
+
+    return {
+        props: {
+            ...(await serverSideTranslations(locale ?? "uz", ["common"])),
+        },
+    };
+};
+export default Index;
