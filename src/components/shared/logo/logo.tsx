e06fd706a003137a08d8fa438450b7e4d82344b7
Вячеслав Тю <vyacheslavtyu@Vaceslavs-MacBook-Pro.local>
2024-01-16 21:48:30 +0500

    fixed

--- src/components/shared/logo/logo.tsx
+++ src/components/shared/logo/logo.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
 import logo from '../../../../public/images/icons/logo.svg'
 import Link from "next/link";
+import css from './logo.module.css'
 
 
 
 const Logo = () => {
     return (
-        <Link href={'/'}>
+        <Link href={'/'} className={css.logo}>
             <ResponsiveImage src={logo} alt={'MGB store'}/>
         </Link>
     );
