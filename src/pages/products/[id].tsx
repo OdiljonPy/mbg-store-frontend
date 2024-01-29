c51cea78133340774c95d1cdd7f6cbf21a1ea661
Вячеслав Тю <vyacheslavtyu@Vaceslavs-MacBook-Pro.local>
2024-01-29 12:25:56 +0500

    fixed

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GetStaticProps} from "next";
+import {GetServerSideProps} from "next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import Wrapper from "@/components/pages/product/wrapper/wrapper";
@@ -25,7 +25,7 @@ const Id = (props: props) => {
 export default Id;
 
 type Props = {};
-export const getStaticProps: GetStaticProps<Props> = async ({locale}) => {
+export const getServerSideProps: GetServerSideProps<Props> = async ({locale}) => {
 
 
     return {
@@ -35,9 +35,3 @@ export const getStaticProps: GetStaticProps<Props> = async ({locale}) => {
     };
 };
 
-export async function getStaticPaths() {
-    const paths = [{
-        params: {id: '1'}
-    }]
-    return {paths, fallback: true}
-}
