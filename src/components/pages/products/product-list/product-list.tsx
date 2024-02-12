bb6aae77655529727c98b88ed7c0d6fb50d31937
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-02-12 14:20:32 +0500

    add new product

--- src/components/pages/products/product-list/product-list.tsx
+++ src/components/pages/products/product-list/product-list.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import css from './product-list.module.css'
 import Product from "@/components/shared/product/product";
-import {product, productWithoutDiscount} from "@/constants/product/product";
+import {product, productClose, productSales, productTop, productWithoutDiscount} from "@/constants/product/product";
 import {useSearchParams} from "next/navigation";
 import {useRouter} from "next/router";
 
@@ -18,18 +18,39 @@ const ProductList = (props: props) => {
     return (
         <>
             <div className={`${css.list} ${isOpened ? css.short: ''}`}>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
-                <Product product={product} isNotSwiper/>
-                <Product product={productWithoutDiscount} isNotSwiper/>
+                {
+                    productSales.map((product)=>{
+                        return(
+                            <Product product={product} isNotSwiper key={product.id}/>
+                        )
+                    })
+                }
+                {
+                    productClose.map((product)=>{
+                        return(
+                            <Product product={product} isNotSwiper key={product.id} />
+                        )
+                    })
+                }
+                {
+                    productTop.map((product) =>{
+                        return(
+                            <Product product={product} isNotSwiper key={product.id}/>
+                        )
+                    })
+                }
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
+                {/*<Product product={product} isNotSwiper/>*/}
+                {/*<Product product={productWithoutDiscount} isNotSwiper/>*/}
             </div>
 
         </>
