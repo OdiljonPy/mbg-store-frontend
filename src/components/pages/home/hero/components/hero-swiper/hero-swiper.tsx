c2f0c831d6211d63bcd171ea35821a95d9516495
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-02-12 14:26:50 +0500

    fix:slider responsive

--- src/components/pages/home/hero/components/hero-swiper/hero-swiper.tsx
+++ src/components/pages/home/hero/components/hero-swiper/hero-swiper.tsx
@@ -20,7 +20,7 @@ const HeroSwiper = (props: props) => {
             "(min-width: 320px)": {
                 slides: {
                     origin: "center",
-                    perView: 1.2,
+                    perView: 1.12,
                     spacing: 15
                 },
             },
