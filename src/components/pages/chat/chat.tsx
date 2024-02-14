d68b9233c9065828f10a1e59ddb85c6e518d123d
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-02-14 15:26:26 +0500

    add animation for scroll top components

--- src/components/pages/chat/chat.tsx
+++ src/components/pages/chat/chat.tsx
@@ -1,16 +1,24 @@
 import css from "./index.module.css"
 import {createPortal} from "react-dom";
 import ScrollTop from "@/components/pages/chat/scroll_top";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const Chat = () =>{
+    const [show,setShow] = useState(false)
   const scrollTop = () =>{
       window.scrollTo({top:0,behavior:'smooth'})
   }
 
+    useEffect(() => {
+        window.addEventListener('scroll',(()=>{
+            if(window.scrollY > 350) setShow(true)
+            else setShow(false)
+        }))
+    }, []);
+
    return(
-       <div className={css.actions}>
-           <ScrollTop onClick={scrollTop} />
+       <div className={css.actions} >
+           <ScrollTop onClick={scrollTop} isShow={show} />
        </div>
    )
 
