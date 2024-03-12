95dd88cff392365144588a38470576c1caa037f5
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-03-12 12:01:40 +0500

    feat: redirect if width > 991

--- src/components/pages/account/profile/wrapper.tsx
+++ src/components/pages/account/profile/wrapper.tsx
@@ -1,4 +1,7 @@
-import { useTranslations } from "next-intl";
+import { fetchUser } from "@/slices/auth/user";
+import { AppDispatch, RootState } from "@/store";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./header/header";
 import PersonalData from "./personal-data/personal-data";
 import Security from "./security/security";
@@ -7,13 +10,30 @@ import css from "./wrapper.module.css";
 interface props {}
 
 const Wrapper = (props: props) => {
-	const t = useTranslations();
+	const { user, loading, error } = useSelector(
+		(state: RootState) => state.user
+	);
+
+	const dispatch = useDispatch<AppDispatch>();
+
+	useEffect(() => {
+		dispatch(fetchUser());
+	}, [dispatch, user]);
+
 	return (
 		<section className={css.profile}>
 			<Header />
 			<div className={css.forms}>
-				<PersonalData />
-				<Security />
+				<PersonalData
+					user={user}
+					loading={loading}
+					error={error}
+				/>
+				<Security
+					user={user}
+					loading={loading}
+					error={error}
+				/>
 			</div>
 		</section>
 	);
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -3,11 +3,21 @@ import AccountWrapper from "@/components/pages/account/wrapper";
 import { GetStaticProps } from "next";
 import { useTranslations } from "next-intl";
 import Head from "next/head";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 interface props {}
 
 const Account = (props: props) => {
 	const t = useTranslations();
+	const { push } = useRouter();
+
+	useEffect(() => {
+		if (screen.width > 991) {
+			push("/account/profile");
+		}
+	}, [push]);
+
 	return (
 		<>
 			<Head>
