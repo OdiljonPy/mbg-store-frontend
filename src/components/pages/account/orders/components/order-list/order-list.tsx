9702ad6e100afb03ceddfbf3c89f970c70c0c276
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-03-21 23:11:47 +0500

    feat: orders empty component

--- src/components/pages/account/orders/components/order-list/order-list.tsx
+++ src/components/pages/account/orders/components/order-list/order-list.tsx
@@ -1,8 +1,13 @@
 import { data } from "../../data";
+import OrdersEmpty from "./orders-empty/orders-empty";
 import OrderItem from "./order-item/order-item";
 import css from "./order-list.module.css";
 
 function OrdersList() {
+	if (!data.length) {
+		return <OrdersEmpty />;
+	}
+
 	return (
 		<ul className={css.list}>
 			{data.map((item) => (
--- src/components/pages/account/orders/wrapper.tsx
+++ src/components/pages/account/orders/wrapper.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/header";
 import OrdersList from "./components/order-list/order-list";
 import Search from "./components/search/search";
+import { data } from "./data";
 
 import css from "./wrapper.module.css";
 
@@ -8,9 +9,11 @@ const Wrapper = () => {
 	return (
 		<section className={css.orders}>
 			<Header />
-			<div className={css.search_box}>
-				<Search />
-			</div>
+			{data.length && (
+				<div className={css.search_box}>
+					<Search />
+				</div>
+			)}
 			<OrdersList />
 		</section>
 	);
