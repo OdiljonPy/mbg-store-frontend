4b6437e8039016bcddc447f01f49e49ef4747fdf
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-03-26 16:34:02 +0500

    fix:router to cart deleivery

--- src/components/pages/cart/total/total.tsx
+++ src/components/pages/cart/total/total.tsx
@@ -3,7 +3,7 @@ import {useTranslations} from "next-intl";
 import TotalItem from "@/components/pages/cart/total/total-item/total-item";
 import {priceFormatter} from "@/utils/price-formatter/price-formatter";
 import Promocode from "@/components/pages/cart/total/promocode/promocode";
-import Link from "next/link";
+import {useRouter} from "next/navigation";
 
 interface props {
 
@@ -11,6 +11,7 @@ interface props {
 
 const Total = (props: props) => {
     const t = useTranslations()
+    const { push } = useRouter();
     return (
         <div className={css.total}>
             <h3 className={css.title}>
@@ -21,11 +22,9 @@ const Total = (props: props) => {
                 <TotalItem className={css.bordered} label={t('cart.sales')} value={priceFormatter(-26000, true)}/>
                 <TotalItem className={css.finalPrice} label={t('cart.actualPrice')}
                            value={priceFormatter(174000 - 26000, true)}/>
-                <Link href={'/cart/delivery'} className={css.link}>
-                <button type={'button'} className={css.btn}>
+                <button type={'button'} onClick={() => push('/cart/delivery')} className={css.btn}>
                     {t('cart.order')}
                 </button>
-                </Link>
                 <Promocode/>
             </div>
         </div>
