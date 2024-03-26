7f9881cab494310d4be809d04877d357fc49c15a
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-03-26 17:16:38 +0500

    fix:delivery phone input

--- src/components/pages/cart/delivery/content/recipient/recipient.tsx
+++ src/components/pages/cart/delivery/content/recipient/recipient.tsx
@@ -1,12 +1,13 @@
+import React, {useState} from "react";
 import css from "./recipient.module.css"
 import {raleway} from "@/constants/fonts/fonts";
 import inputCss from "@/styles/input.module.css";
 import FormError from "@/components/shared/form-error/form-error";
 import { useForm} from "react-hook-form";
-import React, {useState} from "react";
 import {useTranslations} from "next-intl";
-import {IFeedbackForm} from "@/components/pages/feedback/data-types/feedback";
+
 import Heading from "@/components/pages/cart/delivery/content/heading/heading";
+import {PhoneInput} from "react-international-phone";
 interface props{
 
 }
@@ -16,46 +17,46 @@ interface IUserInfo{
 }
 const Recipient = (props:props) =>{
     const t = useTranslations()
+    const [userName,setUserName] = useState("")
+    const [userPhone,setUserPhone] = useState("")
+
     const {resetField, clearErrors, handleSubmit, watch, register, formState: {errors}, control, setValue} = useForm<IUserInfo>({
         mode: 'all'
     })
 
-function getUserData(data:any):void {
-    resetField('name')
-    resetField('phone')
-}
     return(
         <div>
             <Heading title='Получатель заказа' index={1} />
-            <form onSubmit={handleSubmit(getUserData)} className={css.form}>
+            <form  className={css.form}>
                 <div className={css.flex}>
                     <div className={css.field}>
                         <label className={css.label}>
                             {t('cart.delivery.name_label')}
                             <span className={css.required}>*</span>
                         </label>
-                        <input {...register('name', {
-                            required: {
-                                value:true,
-                                message: t('errors.required')
-                            }
-                        })}  placeholder={t('cart.delivery.name_placeholder')}
-                               className={`${inputCss.input}  ${raleway.className} ${errors.name?.message ? inputCss.error : ''} `}/>
-                        <FormError error={errors.name?.message}/>
+                        <input  value={userName} onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUserName(e.target.value)}  placeholder={t('cart.delivery.name_placeholder')}
+                               className={`${inputCss.input}  ${raleway.className} `}/>
+
                     </div>
-                    <div className={css.field}>
-                        <label className={css.label}>
-                            {t('cart.delivery.phone_label')}   <span className={css.required}>*</span>
-                        </label>
-                        <input {...register('phone', {
-                            required: {
-                                value:true,
-                                message: t('errors.required')
-                            }
-                        })}  placeholder='(99) 999-99-99'
-                               className={`${inputCss.input}  ${raleway.className} ${errors.phone?.message ? inputCss.error : ''} `}/>
-                        <FormError error={errors.phone?.message}/>
-                    </div>
+
+                        <div className={css.field}>
+                            <label className={css.label}>
+                                {t('cart.delivery.phone_label')}
+                                <span className={css.required}>*</span>
+                            </label>
+                            <PhoneInput
+                                hideDropdown={true}
+                                inputClassName={inputCss.input}
+                                defaultCountry='uz'
+                                inputProps={{
+                                    placeholder: "+998",
+                                }}
+                                placeholder={"Номер телефона"}
+                                value={userPhone}
+                                onChange={(phone) => setUserPhone(phone)}
+                            />
+                            <FormError error={errors.phone?.message}/>
+                        </div>
                 </div>
             </form>
         </div>
