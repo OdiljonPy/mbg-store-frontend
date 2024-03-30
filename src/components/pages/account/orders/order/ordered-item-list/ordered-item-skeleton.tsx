349dccecb400d8c105f89475c693396ef8fcc569
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-03-30 22:27:33 +0500

    feat: get order data from backend

---
+++ src/components/pages/account/orders/order/ordered-item-list/ordered-item-skeleton.tsx
@@ -0,0 +1,41 @@
+import Skeleton from "react-loading-skeleton";
+import css from "./ordered-item/ordered-item.module.css";
+
+function OrderedItemSkeleton() {
+	return (
+		<div className={css.wrapper}>
+			<div className={css.image_box}>
+				<Skeleton
+					width={90}
+					height={100}
+				/>
+			</div>
+			<div className={css.info}>
+				<div className={css.info_left}>
+					<div>
+						<Skeleton width={200} />
+						<Skeleton
+							width={100}
+							height={10}
+						/>
+					</div>
+					<p className={css.seller}>
+						<Skeleton width={250} />
+					</p>
+				</div>
+				<div className={css.info_right}>
+					<Skeleton width={120} />
+					<Skeleton
+						width={100}
+						height={10}
+					/>
+					<p className={css.count}>
+						<Skeleton width={80} />
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default OrderedItemSkeleton;
