61a545fae93aec4e636d012c304ff447e17dad9e
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-04-02 12:07:45 +0500

    refactore:code

--- src/slices/basket/basketSlice.ts
+++ src/slices/basket/basketSlice.ts
@@ -1,8 +1,6 @@
 
 import {IProduct} from "@/data-types/products/common";
 import {createSlice} from "@reduxjs/toolkit";
-import {getLocaleStorage, saveLocalStorage} from "@/utils/save-product-localstorage/localStorage";
-import {Joan} from "next/dist/compiled/@next/font/dist/google";
 
 const initialState = {
     products : [] as IProduct[]
@@ -13,40 +11,15 @@ const basketSlices = createSlice({
     initialState,
     reducers:{
         addProduct : (state, {payload}) =>{
-                // const product = localStorage.getItem('basket')
-                // if(product){
-                //     const activeProduct:IProduct = JSON.parse(product).find((product:IProduct) => product.id === payload.id)
-                //     const newCount = activeProduct?.count ? activeProduct.count + 1  : 1
-                //     state.products = state.products.filter((product) => product.id !== payload.id)
-                //     state.products.push({count:newCount,...payload})
-                // }
-                // else{
-                //     state.products.push({count:1,...payload})
-                // }
-                // saveLocalStorage(state.products)
-
             const index = state.products.findIndex(product => product.id === payload.id);
             if (index !== -1) {
                 state.products[index].count += payload.count;
             } else {
-                // Product doesn't exist, add new
                 state.products.push(payload);
             }
         },
 
         removeProduct : (state, {payload}) =>{
-            // const product = localStorage.getItem('basket')
-            // if(product){
-            //     const activeProduct:IProduct = JSON.parse(product).find((product:IProduct) => product.id === payload.id)
-            //     const newCount = activeProduct?.count ? activeProduct.count - 1  : 0
-            //     if(newCount){
-            //          state.products.push({count:newCount,...payload})
-            //     }
-            //     else
-            //          state.products = state.products.filter((product) => product.id !== payload.id)
-            // }
-            //
-            // saveLocalStorage(state.products)
             state.products = state.products.filter(product => product.id !== payload.id);
         }
     }
