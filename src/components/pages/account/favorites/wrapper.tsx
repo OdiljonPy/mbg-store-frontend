41addd956973b80e34b1a74c054e31590f6e1431
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-04-03 16:54:53 +0500

    fix:build error

--- src/components/pages/account/favorites/wrapper.tsx
+++ src/components/pages/account/favorites/wrapper.tsx
@@ -8,12 +8,12 @@ import css from "./wrapper.module.css";
 export type AddressBookTab = "delivery" | "pickup";
 
 const Wrapper = () => {
-	const favorites = useSelector((state: RootState) => state.favorites);
+	const {favourites} = useSelector((state: RootState) => state.favorites);
 
 	return (
 		<section className={css.wrapper}>
 			<Header />
-			{favorites.length ? (
+			{favourites.length ? (
 				<div className={css.search_box}>
 					<Search />
 				</div>
