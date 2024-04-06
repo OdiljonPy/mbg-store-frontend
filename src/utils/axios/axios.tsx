2c34b8c181f746af3d2ee6a7eef722f1e993c66f
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-04-06 18:31:22 +0500

    feat: logout if token is expired

--- src/utils/axios/axios.tsx
+++ src/utils/axios/axios.tsx
@@ -1,18 +1,29 @@
 import axios from "axios";
 
 const defaultOptions = {
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    // },
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+	// headers: {
+	//     'Content-Type': 'application/json',
+	// },
 };
 
 const API = axios.create(defaultOptions);
 
 API.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('access_token');
-    config.headers.Authorization = token ? `Bearer ${token}` : '';
-    return config;
+	const token = localStorage.getItem("access_token");
+	config.headers.Authorization = token ? `Bearer ${token}` : "";
+	return config;
 });
+
+API.interceptors.response.use(
+	(config) => config,
+	async (error) => {
+		if (error.response.status === 401 && error.config) {
+			localStorage.removeItem("access_token");
+			localStorage.removeItem("refresh_token");
+			window.location.href = "/";
+		}
+	}
+);
 
 export default API;
