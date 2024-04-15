939fe0fa512ebfab97662469ad345086390a37c1
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-04-15 17:31:36 +0500

    style: top header

--- src/layout/components/header/top-header/top-header.tsx
+++ src/layout/components/header/top-header/top-header.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import css from './top-header.module.css'
-import {Flex} from "antd";
+import React from "react";
+import css from "./top-header.module.css";
+import { Flex } from "antd";
 import AddressSelect from "@/layout/components/header/top-header/components/address-select/address-select";
 import Nav from "@/layout/components/header/top-header/components/nav/nav";
 
-interface props {
-
-}
+interface props {}
 
 const TopHeader = (props: props) => {
-    return (
-        <div className={css.header}>
-            <div className={'container'}>
-                <Flex justify={'space-between'} align={'center'} gap={'small'}>
-                    <AddressSelect/>
-                    <Nav/>
-                </Flex>
-            </div>
-        </div>
-    );
+	return (
+		<div className={css.header}>
+			<div className={"container"}>
+				<div className={css.wrapper}>
+					<AddressSelect />
+					<Nav />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TopHeader;
