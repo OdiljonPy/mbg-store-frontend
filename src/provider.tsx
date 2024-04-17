4748652be4502754c733e72b086618bae68c9be5
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-04-17 15:33:13 +0500

    feat: yandex metrica

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,12 +1,24 @@
 "use client";
+import { store } from "@/store";
+import { YandexMetricaProvider } from "next-yandex-metrica";
 import { Provider } from "react-redux";
-import { store} from "@/store";
-import { ToastProvider } from 'react-toast-notifications';
+import { ToastProvider } from "react-toast-notifications";
 
-export function Providers({children} : {children:React.ReactNode}){
-    return <ToastProvider>
-                <Provider store={store}>
-                         {children}
-                </Provider>
-            </ToastProvider>
+export function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<YandexMetricaProvider
+			tagID={97050233}
+			initParameters={{
+				clickmap: true,
+				trackLinks: true,
+				accurateTrackBounce: true,
+				webvisor: true,
+				ecommerce: "dataLayer",
+			}}
+		>
+			<ToastProvider>
+				<Provider store={store}>{children}</Provider>
+			</ToastProvider>
+		</YandexMetricaProvider>
+	);
 }
