80538a0d386f579758bb41869e6e0a6f44ad6968
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-04-18 17:33:45 +0500

    fix:product feedback hanf=dle

--- src/components/pages/feedback/components/feedback-form/rates/rates.tsx
+++ src/components/pages/feedback/components/feedback-form/rates/rates.tsx
@@ -4,22 +4,25 @@ import Rate from "@/components/pages/feedback/components/feedback-form/rates/rat
 import {useTranslations} from "next-intl";
 import {UseFormRegister, UseFormWatch} from "react-hook-form";
 import {IFeedbackForm} from "@/components/pages/feedback/data-types/feedback";
+import FormError from "@/components/shared/form-error/form-error";
 
 interface props {
     register: UseFormRegister<IFeedbackForm>
     watch: UseFormWatch<IFeedbackForm>
+    errors:string | undefined
 }
 
 const rates: number[] = [1, 2, 3, 4, 5]
 
-const Rates = ({register, watch}: props) => {
+const Rates = ({register, watch,errors}: props) => {
     const t = useTranslations()
     return (
         <span className={css.wrapper}>
                 <span className={css.rating}>
                 {rates.map((rate) => (
-                    <Rate key={rate} register={register} watch={watch} rate={rate}/>
+                    <Rate key={rate} register={register} watch={watch} rate={rate} errors={errors}/>
                 ))}
+                    <FormError error={errors}/>
                 </span>
                 <span className={css.labels}>
                     <span>
