a3bba1b3b9c890f048e559bf455da205399d9433
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-03 17:08:59 +0500

    add product comment lightbox

--- src/components/pages/product/wrapper/components/feedbacks/list/feedback/components/question.tsx
+++ src/components/pages/product/wrapper/components/feedbacks/list/feedback/components/question.tsx
@@ -1,15 +1,40 @@
+import {StaticImageData} from "next/image";
+import {SlideshowLightbox} from 'lightbox.js-react'
+import 'lightbox.js-react/dist/index.css'
+
 import css from "@/components/pages/product/wrapper/components/feedbacks/list/feedback/feedback.module.css";
 import {formatDate} from "@/utils/format-date/format-date";
 import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
 import star from "../../../../../../../../../../public/images/icons/star.svg";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {IComments} from "@/data-types/products/common";
+
 
 interface props {
     feedback: IComments
 }
+
+interface ILightBoxImages{
+    src:string | StaticImageData,
+    alt:string|number
+}
 const Question = ({feedback}:props) =>{
-    const {name, rating, comment, images,created_at,answers} = feedback
+    const {name, rating, comment, images,created_at} = feedback
+    const [isOpen, setIsOpen] = useState(false);
+    const [lightBoxImages,setLightBoxImages] = useState<ILightBoxImages[]>([])
+
+    useEffect(() => {
+        if(images?.length){
+            const image:ILightBoxImages[] = images.map((image)=> {
+                return {
+                    src:image?.image,
+                    alt:image?.id
+                }
+            })
+
+            setLightBoxImages(image)
+        }
+    }, [feedback]);
     return(
         <div>
             <div className={css.header}>
@@ -35,13 +60,21 @@ const Question = ({feedback}:props) =>{
                 {
                     images?.length ?  <div className={css.images}>
                         {images?.map((item) => (
-                            <div key={item?.id} className={css.img}>
+                            <div key={item?.id} className={css.img} onClick={()=> setIsOpen(true)}>
                                 <ResponsiveImage src={item?.image} alt={''}/>
                             </div>
                         ))}
                     </div> : ''
                 }
             </div>
+            <SlideshowLightbox
+                backgroundColor={'rgba(0,0,0,0.5)'}
+                images={lightBoxImages}
+                showThumbnails={true}
+                open={isOpen}
+                lightboxIdentifier="lbox1"
+                onClose={() =>{setIsOpen(false)}}>
+            </SlideshowLightbox>
         </div>
     )
 }
