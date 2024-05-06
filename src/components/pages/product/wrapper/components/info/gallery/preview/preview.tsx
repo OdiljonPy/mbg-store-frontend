d262faa6e4e4e488c1eb3e2b3b9a378595d47c1d
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-06 18:13:32 +0500

    feat: add product flag for product

--- src/components/pages/product/wrapper/components/info/gallery/preview/preview.tsx
+++ src/components/pages/product/wrapper/components/info/gallery/preview/preview.tsx
@@ -1,41 +1,69 @@
-import React, {ForwardedRef, forwardRef} from 'react';
 import css from "@/components/pages/product/wrapper/components/info/gallery/thumbnail/thumbnail.module.css";
-import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
-import mainCss from './preview.module.css'
 import Badge from "@/components/shared/badge/badge";
-import {useTranslations} from 'next-intl';
-import {IProductInner} from "@/data-types/products/product-inner/product-inner";
+import { productFlagMap } from "@/components/shared/product/components/product-top/product-flag-map";
+import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
+import { IProductInner } from "@/data-types/products/product-inner/product-inner";
+import { useTranslations } from "next-intl";
+import { ForwardedRef, forwardRef } from "react";
+import mainCss from "./preview.module.css";
 
 interface props {
-    gallery: IProductInner,
-    loading:boolean
+	gallery: IProductInner;
+	loading: boolean;
 }
 
-const Preview = forwardRef(({loading,gallery}: props, ref: ForwardedRef<HTMLDivElement>) => {
-    const t = useTranslations()
+const Preview = forwardRef(
+	({ loading, gallery }: props, ref: ForwardedRef<HTMLDivElement>) => {
+		const t = useTranslations();
 
-    return (
-        <div ref={ref} className={`keen-slider ${mainCss.wrapper}`}>
+		return (
+			<div ref={ref} className={`keen-slider ${mainCss.wrapper}`}>
+				{loading
+					? ""
+					: gallery?.images?.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className={`${css.item} ${mainCss.img} keen-slider__slide`}
+								>
+									{gallery?.available < 1 && (
+										<div className={css.badge}>
+											<Badge
+												text={t("products.sold")}
+												color={"#767BF9"}
+											/>
+										</div>
+									)}
 
-            {
-                loading ? '' : gallery?.images?.map((item) =>{
-                    return (
-                      <div key={item.id} className={`${css.item} ${mainCss.img} keen-slider__slide`}>
-                          {
-                              gallery?.available < 1 &&  <div className={css.badge}>
-                                  <Badge text={t('products.sold')} color={'#767BF9'}/>
-                              </div>
-                          }
+									<div className={css.badge}>
+										{gallery?.available > 0 &&
+										gallery?.flag ? (
+											<Badge
+												text={t(
+													productFlagMap[
+														gallery?.flag
+													]
+												)}
+												color={"#767BF9"}
+											/>
+										) : (
+											""
+										)}
+									</div>
 
-                           <ResponsiveImage src={item.image} alt={'Кукуруза Bonduelle Classique сладкая'}/>
-                       </div>
-                    )
-                })
-            }
-        </div>
-    );
-})
+									<ResponsiveImage
+										src={item.image}
+										alt={
+											"Кукуруза Bonduelle Classique сладкая"
+										}
+									/>
+								</div>
+							);
+					  })}
+			</div>
+		);
+	}
+);
 
-
-Preview.displayName = 'Preview'
+Preview.displayName = "Preview";
 export default Preview;
