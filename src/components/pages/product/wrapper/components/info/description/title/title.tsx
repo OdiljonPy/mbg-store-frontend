2867fa840f846571f060bd800749955c63637af4
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-08 16:13:09 +0500

    add product inner preloading

--- src/components/pages/product/wrapper/components/info/description/title/title.tsx
+++ src/components/pages/product/wrapper/components/info/description/title/title.tsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import css from './title.module.css'
+import React from "react";
+import css from "./title.module.css";
 import Skeleton from "react-loading-skeleton";
 interface props {
-title: string
-loading?:boolean
-skeletonWidth?:number
+  title: string;
 }
 
-const Title = ({title,loading,skeletonWidth}: props) => {
-    return (
-        <h1 className={css.title}>
-            {loading ? <Skeleton count={1} height={'40px'} width={skeletonWidth ? `${skeletonWidth}px` :'100%'}  /> : title}
-        </h1>
-    );
+const Title = ({ title }: props) => {
+  return <h1 className={css.title}>{title}</h1>;
 };
 
 export default Title;
