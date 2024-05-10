c91e3a2982e1713e078fa85b09df067afbaf11be
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-10 15:31:48 +0500

    fix:flag position

--- src/components/shared/product/components/product-top/product-top.tsx
+++ src/components/shared/product/components/product-top/product-top.tsx
@@ -10,60 +10,61 @@ import { productFlagMap } from "./product-flag-map";
 import css from "./product-top.module.css";
 
 const ClientAddToFav = dynamic(
-	() => import("@/components/shared/add-to-fav/add-to-fav"),
-	{
-		ssr: false,
-	}
+  () => import("@/components/shared/add-to-fav/add-to-fav"),
+  {
+    ssr: false,
+  },
 );
 
 interface props {
-	product: IProduct;
+  product: IProduct;
 }
 
 const ProductTop = ({ product }: props) => {
-	const t = useTranslations();
-	const { id, images, name, discount, count, available } = product;
+  const t = useTranslations();
+  const { id, images, name, discount, count, available } = product;
 
-	return (
-		<div className={css.actions}>
-			<div className={css.discount}>
-				{available > 1 && discount ? (
-					<DiscountBadge discount_percentage={discount} />
-				) : (
-					""
-				)}
-			</div>
-			<div className={css.unavailable}>
-				{available < 1 ? (
-					<Badge text={t("products.sold")} color={"#767BF9"} />
-				) : (
-					""
-				)}
-			</div>
+  return (
+    <div className={css.actions}>
+      <div className={css.discount}>
+        {available > 1 && discount ? (
+          <DiscountBadge discount_percentage={discount} />
+        ) : (
+          ""
+        )}
+      </div>
+      <div className={css.unavailable}>
+        {available < 1 ? (
+          <Badge text={t("products.sold")} color={"#767BF9"} />
+        ) : (
+          ""
+        )}
+      </div>
 
-			<div className={css.unavailable}>
-				{available > 0 && product.flag ? (
-					<Badge
-						text={t(productFlagMap[product.flag])}
-						color={"#767BF9"}
-					/>
-				) : (
-					""
-				)}
-			</div>
-			{available > 0 ? (
-				<div className={css.favorite_icon}>
-					<ClientAddToFav product={product} />
-				</div>
-			) : (
-				""
-			)}
-			<Link href={`/products/${id}`} className={css.img}>
-				<ResponsiveImage src={images[0]?.image} alt={name} />
-			</Link>
-			<ProductActions count={count} product={product} />
-		</div>
-	);
+      <div className={`${css.flag} ${discount ? css.flag_with_discount : ""}`}>
+        {available > 0 && product.flag ? (
+          <Badge
+            className={css.flag_badge}
+            text={t(productFlagMap[product.flag])}
+            color={"#767BF9"}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      {available > 0 ? (
+        <div className={css.favorite_icon}>
+          <ClientAddToFav product={product} />
+        </div>
+      ) : (
+        ""
+      )}
+      <Link href={`/products/${id}`} className={css.img}>
+        <ResponsiveImage src={images[0]?.image} alt={name} />
+      </Link>
+      <ProductActions count={count} product={product} />
+    </div>
+  );
 };
 
 export default ProductTop;
