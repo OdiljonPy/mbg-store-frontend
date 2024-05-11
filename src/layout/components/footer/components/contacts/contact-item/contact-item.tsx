79edc65b1f217a0bc9181fae43bd23a126bb6855
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-11 15:15:16 +0500

    fix:footer link and social

--- src/layout/components/footer/components/contacts/contact-item/contact-item.tsx
+++ src/layout/components/footer/components/contacts/contact-item/contact-item.tsx
@@ -7,32 +7,53 @@ import Skeleton from "react-loading-skeleton";
 import css from "./contact-item.module.css";
 
 interface props {
-	icon: StaticImageData;
-	label: string;
-	path: string;
-	title: string;
-	loading: boolean;
+  icon: StaticImageData;
+  label: string;
+  path: string;
+  title: string;
+  loading: boolean;
+  type?: "phone" | "mail" | "default";
 }
 
-const ContactItem = ({ icon, label, path, title , loading}: props) => {
-	const t = useTranslations();
+const ContactItem = ({
+  icon,
+  label,
+  path,
+  title,
+  loading,
+  type = "default",
+}: props) => {
+  const t = useTranslations();
 
-	if (loading) return <div className={css.item}>
-		<Skeleton width={100} height={20} />
-		<Skeleton width={300} height={20} />
-	</div>
-	
-	return (
-			<Link href={path} className={css.item}>
-				<div className={css.top}>
-					<span className={css.icon}>
-						<ResponsiveImage src={icon} alt={title} />
-					</span>
-					<span className={css.label}>{t(label)}</span>
-				</div>
-				<div className={css.text}>{title}</div>
-			</Link>
-	);
+  if (loading)
+    return (
+      <div className={css.item}>
+        <Skeleton width={100} height={20} />
+        <Skeleton width={300} height={20} />
+      </div>
+    );
+
+  return (
+    <a
+      href={
+        type == "phone"
+          ? `tel:${path}`
+          : type == "mail"
+            ? `mailto:${path}`
+            : path
+      }
+      target="_blank"
+      className={css.item}
+    >
+      <div className={css.top}>
+        <span className={css.icon}>
+          <ResponsiveImage src={icon} alt={title} />
+        </span>
+        <span className={css.label}>{t(label)}</span>
+      </div>
+      <div className={css.text}>{title}</div>
+    </a>
+  );
 };
 
 export default ContactItem;
