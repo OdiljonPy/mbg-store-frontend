c88fc4750249968693aca8c8161556ea34f3180f
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-11 14:51:43 +0500

    fix:about partner

--- src/components/pages/about/partners/partners-slider/slider-item/slider-item.tsx
+++ src/components/pages/about/partners/partners-slider/slider-item/slider-item.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import css from './slider-item.module.css'
+import React from "react";
+import css from "./slider-item.module.css";
 import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
-import partner from '@/../public/images/partners/mock.svg'
-import {IPartner} from "@/data-types/base/partner";
+import partner from "@/../public/images/partners/mock.svg";
+import { IPartner } from "@/data-types/base/partner";
 interface Props {
-    partner:IPartner
+  partner: IPartner;
 }
 
-const SliderItem = ({partner}: Props) => {
-    return (
-        <div className={`${css.img} keen-slider__slide`}>
-            <ResponsiveImage src={partner?.logo} alt={partner?.partner_name}/>
-        </div>
-    );
+const SliderItem = ({ partner }: Props) => {
+  return (
+    <div className={`${css.img} keen-slider__slide`}>
+      <ResponsiveImage
+        src={partner?.logo}
+        alt={partner?.partner_name}
+        objectFit={"cover"}
+        isStretch
+      />
+    </div>
+  );
 };
 
 export default SliderItem;
