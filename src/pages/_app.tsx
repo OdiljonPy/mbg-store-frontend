54782b6cc0ce94a7e736b503a869e0534d15fa3c
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-15 17:07:07 +0500

    feat: metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,58 +1,64 @@
-import '@/styles/globals.css'
-import '@/styles/roots.css'
-import type {AppProps} from 'next/app'
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
+import "@/styles/globals.css";
+import "@/styles/roots.css";
+import type { GetStaticProps, NextPage } from "next";
+import type { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
 
-import NextNProgress from 'nextjs-progressbar';
 import Layout from "@/layout/layout";
-import { IntlProvider} from 'next-intl';
-import {useRouter} from "next/router";
-import {YMaps} from "@pbe/react-yandex-maps";
-import {Providers} from "@/provider";
+import { Providers } from "@/provider";
+import { YMaps } from "@pbe/react-yandex-maps";
+import { IntlProvider } from "next-intl";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
+	getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
+	Component: NextPageWithLayout;
+};
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+	const { locale } = useRouter();
+	const getLayout = Component.getLayout ?? ((page) => page);
+
+	return (
+		<Providers>
+			<IntlProvider
+				locale={locale!}
+				messages={pageProps.messages}
+				onError={() => {}}
+				timeZone={"Asia/Tashkent"}
+			>
+				<YMaps
+					enterprise
+					query={{
+						lang: "ru_RU",
+						apikey: process.env.NEXT_PUBLIC_YANDEX_KEY,
+						mode: "debug",
+						suggest_apikey: process.env.NEXT_PUBLIC_SUGGEST_KEY,
+					}}
+				>
+					<NextNProgress
+						color={`#39B969`}
+						startPosition={0.3}
+						stopDelayMs={200}
+						height={8}
+						showOnShallow={true}
+					/>
+					{Component.getLayout ? (
+						getLayout(<Component {...pageProps} />)
+					) : (
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					)}
+				</YMaps>
+			</IntlProvider>
+		</Providers>
+	);
 }
 
-function App({Component, pageProps}: AppPropsWithLayout) {
-    const {locale} = useRouter()
-    const getLayout = Component.getLayout ?? ((page) => page)
-
-    return (
-        <Providers>
-        <IntlProvider
-            locale={locale!}
-            messages={pageProps.messages}
-            onError={() => {
-            }}
-            timeZone={'Asia/Tashkent'}
-        >
-            <YMaps enterprise query={{lang: 'ru_RU', apikey: process.env.NEXT_PUBLIC_YANDEX_KEY, mode: 'debug', suggest_apikey: process.env.NEXT_PUBLIC_SUGGEST_KEY}}>
-                        <NextNProgress color={`#39B969`}
-                                       startPosition={0.3}
-                                       stopDelayMs={200}
-                                       height={8}
-                                       showOnShallow={true}/>
-                {
-                    Component.getLayout ? getLayout(
-                        <Component {...pageProps} />
-                    ) : <Layout>
-                        <Component {...pageProps} />
-                    </Layout>
-                }
-            </YMaps>
-
-        </IntlProvider>
-        </Providers>
-    )
-
-}
-
-
-export default App
+export default App;
 
