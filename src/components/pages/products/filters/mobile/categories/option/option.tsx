4d894325a7d2eef9cc36846398cb1cd89f59e62b
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-15 17:12:21 +0500

    fix: category object key from "icone" to icon

--- src/components/pages/products/filters/mobile/categories/option/option.tsx
+++ src/components/pages/products/filters/mobile/categories/option/option.tsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import css from './option.module.css'
-import {useFormContext} from "react-hook-form";
-import {IFilters} from "@/components/pages/products/filters/mobile/mobile-filters/data-types";
+import { IFilters } from "@/components/pages/products/filters/mobile/mobile-filters/data-types";
 import ResponsiveImage from "@/components/shared/responsive-image/responsive-image";
-import {ICategory} from "@/data-types/categories/categories";
+import { ICategory } from "@/data-types/categories/categories";
+import { useFormContext } from "react-hook-form";
+import css from "./option.module.css";
 
 interface props {
-    item: ICategory
-    onClose: () => void
+	item: ICategory;
+	onClose: () => void;
 }
 
-const Option = ({item, onClose}: props) => {
-    const {id, name, count_product, icone} = item
-    const {
-        watch,
-        setValue
-    } = useFormContext<IFilters>()
+const Option = ({ item, onClose }: props) => {
+	const { id, name, count_product, icon: icon } = item;
+	const { watch, setValue } = useFormContext<IFilters>();
 
-    const onSetValue = () => {
-        setValue('category', id.toString())
-        onClose()
-    }
+	const onSetValue = () => {
+		setValue("category", id.toString());
+		onClose();
+	};
 
-    const category = watch('category')
+	const category = watch("category");
 
-    return (
-        <label onClick={onSetValue} className={css.item}>
-            <input type={'radio'} className={css.input} value={id}/>
-            <span className={css.info}>
-               <span className={css.icon}>
-                    <ResponsiveImage src={icone} alt={name}/>
-               </span>
-                <span className={`${css.label} text-mobile-size ${category === id.toString() ? css.active: ''}`}>
-                 {name}
-                </span>
-            </span>
-            <span className={`${css.count} text-mobile-size`}>
-                {count_product}
-            </span>
-        </label>
-    );
+	return (
+		<label onClick={onSetValue} className={css.item}>
+			<input type={"radio"} className={css.input} value={id} />
+			<span className={css.info}>
+				<span className={css.icon}>
+					<ResponsiveImage src={icon} alt={name} />
+				</span>
+				<span
+					className={`${css.label} text-mobile-size ${
+						category === id.toString() ? css.active : ""
+					}`}
+				>
+					{name}
+				</span>
+			</span>
+			<span className={`${css.count} text-mobile-size`}>
+				{count_product}
+			</span>
+		</label>
+	);
 };
 
 export default Option;
