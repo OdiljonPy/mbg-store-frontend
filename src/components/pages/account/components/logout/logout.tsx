601925db74f5cf77853ba225a660ec06467ad57b
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-16 15:42:00 +0500

    fix:localstorage cleared if logged out

--- src/components/pages/account/components/logout/logout.tsx
+++ src/components/pages/account/components/logout/logout.tsx
@@ -6,65 +6,61 @@ import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import css from "./logout.module.css";
+import { deletePromoCode } from "@/slices/basket/basketSlice";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Logout = ({ children, ...props }: Props) => {
-	const { open, onClose, onOpen } = useModal();
-	const t = useTranslations();
-	const { push } = useRouter();
-	const dispatch = useDispatch<AppDispatch>();
+  const { open, onClose, onOpen } = useModal();
+  const t = useTranslations();
+  const { push } = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
 
-	const handleLogout = () => {
-		dispatch(logoutUser());
-		push("/");
-	};
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    dispatch(deletePromoCode());
+    push("/");
+  };
 
-	return (
-		<>
-			<button
-				{...props}
-				onClick={onOpen}
-			>
-				{children}
-			</button>
-			<Modal
-				destroyOnClose={true}
-				open={open}
-				onCancel={onClose}
-				closeIcon={false}
-				footer={null}
-				width={400}
-				centered
-				style={{ borderRadius: "26px", overflow: "auto" }}
-			>
-				<div className={css.modal}>
-					<div className={css.modal_header}>
-						<h3 className={css.modal_title}>
-							{t("profile.logout.modal_title")}
-						</h3>
-						<p className={css.modal_description}>
-							{t("profile.logout.modal_description")}
-						</p>
-					</div>
-					<div className={css.modal_footer}>
-						<button
-							className={css.btn}
-							onClick={onClose}
-						>
-							{t("profile.logout.modal_cancel")}
-						</button>
-						<button
-							onClick={handleLogout}
-							className={[css.btn, css.logout_btn].join(" ")}
-						>
-							{t("profile.logout.modal_logout")}
-						</button>
-					</div>
-				</div>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <button {...props} onClick={onOpen}>
+        {children}
+      </button>
+      <Modal
+        destroyOnClose={true}
+        open={open}
+        onCancel={onClose}
+        closeIcon={false}
+        footer={null}
+        width={400}
+        centered
+        style={{ borderRadius: "26px", overflow: "auto" }}
+      >
+        <div className={css.modal}>
+          <div className={css.modal_header}>
+            <h3 className={css.modal_title}>
+              {t("profile.logout.modal_title")}
+            </h3>
+            <p className={css.modal_description}>
+              {t("profile.logout.modal_description")}
+            </p>
+          </div>
+          <div className={css.modal_footer}>
+            <button className={css.btn} onClick={onClose}>
+              {t("profile.logout.modal_cancel")}
+            </button>
+            <button
+              onClick={handleLogout}
+              className={[css.btn, css.logout_btn].join(" ")}
+            >
+              {t("profile.logout.modal_logout")}
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default Logout;
