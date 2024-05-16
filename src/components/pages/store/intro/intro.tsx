89074d3b701ef1381e14ddbcd0b258d47c8d916c
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-16 14:57:32 +0500

    fix:store single filter

--- src/components/pages/store/intro/intro.tsx
+++ src/components/pages/store/intro/intro.tsx
@@ -1,31 +1,21 @@
-import React, {useEffect} from 'react';
-import css from './intro.module.css'
+import React from "react";
+import css from "./intro.module.css";
 import Info from "@/components/pages/store/intro/info/info";
 import Deliveries from "@/components/pages/store/intro/deliveries/deliveries";
-import {useRouter} from "next/router";
-import {useDispatch, useSelector} from "react-redux";
-import {AppDispatch, RootState} from "@/store";
-import {fetchStoreSingle} from "@/slices/all_store/StoriesSlices";
+import { IStores } from "@/data-types/stores/stores";
 
 interface props {
-
+  store: IStores;
+  loading: boolean;
 }
 
-const Intro = ({}: props) => {
-    const {query:{id}} = useRouter()
-    const {loading,single_store} = useSelector((state:RootState)=> state.all_stories)
-    const dispatch = useDispatch<AppDispatch>()
-
-    useEffect(() => {
-        dispatch(fetchStoreSingle(id))
-    }, [dispatch]);
-
-    return (
-        <section className={css.info}>
-            <Info store={single_store} loading={loading}/>
-            <Deliveries/>
-        </section>
-    );
+const Intro = ({ store, loading }: props) => {
+  return (
+    <section className={css.info}>
+      <Info store={store} loading={loading} />
+      <Deliveries />
+    </section>
+  );
 };
 
 export default Intro;
