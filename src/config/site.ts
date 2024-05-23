03045c007b49b88c975bd9b20afc229e35905572
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-23 17:38:33 +0500

    feat: add description to metadata

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
 	name: "MBG Store",
-	description: "",
+	description:
+		"Широкий ассортимент товаров со скидками: еда, овощи и фрукты, молочные продукты, мясные продукты, напитки, замороженные продукты, всё для дома и многое другое. Быстрая доставка. Ежедневные скидки и акции на MBG Store!",
 	url: "https://www.mbgstore.uz",
 	ogImage: "https://www.mbgstore.uz/og.png",
 } as const;
