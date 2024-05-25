4de5b703eb704dc8774a22d792f9c937f28ebf1a
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-25 14:48:03 +0500

    feat: not available image if product does not have image

--- src/components/pages/product/wrapper/components/info/gallery/gallery.tsx
+++ src/components/pages/product/wrapper/components/info/gallery/gallery.tsx
@@ -1,98 +1,105 @@
-import React, { useEffect, useState } from "react";
-import css from "./gallery.module.css";
-import { useKeenSlider } from "keen-slider/react";
-import { ThumbnailPlugin } from "@/components/pages/product/wrapper/plugins/thumbnail/thumbnail";
-import Thumbnail from "@/components/pages/product/wrapper/components/info/gallery/thumbnail/thumbnail";
-import Preview from "@/components/pages/product/wrapper/components/info/gallery/preview/preview";
-import "keen-slider/keen-slider.min.css";
-import NavigationBtn from "@/components/pages/product/wrapper/components/info/gallery/navigation-btn/navigation-btn";
 import Dots from "@/components/pages/product/wrapper/components/info/gallery/dots/dots";
-import Skeleton from "react-loading-skeleton";
+import NavigationBtn from "@/components/pages/product/wrapper/components/info/gallery/navigation-btn/navigation-btn";
+import Preview from "@/components/pages/product/wrapper/components/info/gallery/preview/preview";
+import Thumbnail from "@/components/pages/product/wrapper/components/info/gallery/thumbnail/thumbnail";
+import { ThumbnailPlugin } from "@/components/pages/product/wrapper/plugins/thumbnail/thumbnail";
 import { IProductInner } from "@/data-types/products/product-inner/product-inner";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import css from "./gallery.module.css";
 
 interface props {
-  gallery: IProductInner;
-  loading: boolean;
+	gallery: IProductInner;
+	loading: boolean;
 }
 const Gallery = ({ gallery, loading }: props) => {
-  const [currentSlide, setCurrentSlide] = useState<number>(0);
-  const [loaded, setLoaded] = useState<boolean>(false);
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slides: {
-      perView: 1,
-      spacing: 12,
-    },
-    created: () => setLoaded(true),
-    slideChanged: (slider) => {
-      setCurrentSlide(slider.track.details.rel);
-    },
-  });
-  const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
-    {
-      initial: 0,
-      vertical: true,
-      slides: {
-        perView: 3,
-        spacing: 12,
-      },
-    },
-    [ThumbnailPlugin(instanceRef)],
-  );
+	const [currentSlide, setCurrentSlide] = useState<number>(0);
+	const [loaded, setLoaded] = useState<boolean>(false);
+	const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+		initial: 0,
+		slides: {
+			perView: 1,
+			spacing: 12,
+		},
+		created: () => setLoaded(true),
+		slideChanged: (slider) => {
+			setCurrentSlide(slider.track.details.rel);
+		},
+	});
+	const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+		{
+			initial: 0,
+			vertical: true,
+			slides: {
+				perView: 3,
+				spacing: 12,
+			},
+		},
+		[ThumbnailPlugin(instanceRef)]
+	);
 
-  const onPrev = () => {
-    if (gallery?.images?.length > 2) instanceRef?.current?.prev();
-  };
+	const onPrev = () => {
+		if (gallery?.images?.length > 2) instanceRef?.current?.prev();
+	};
 
-  const onNext = () => {
-    if (gallery?.images?.length > 2) instanceRef?.current?.next();
-  };
+	const onNext = () => {
+		if (gallery?.images?.length > 2) instanceRef?.current?.next();
+	};
 
-  const onChangeSlide = (i: number) => instanceRef.current?.moveToIdx(i);
+	const onChangeSlide = (i: number) => instanceRef.current?.moveToIdx(i);
 
-  useEffect(() => {
-    const slider = instanceRef.current;
-    return () => {
-      slider?.update();
-    };
-  }, [instanceRef, thumbnailRef, sliderRef]);
+	useEffect(() => {
+		const slider = instanceRef.current;
+		return () => {
+			slider?.update();
+		};
+	}, [instanceRef, thumbnailRef, sliderRef]);
 
-  return (
-    <>
-      {loading ? (
-        <Skeleton
-          count={1}
-          className={css.skeleton}
-          containerClassName={css.skeleton_container}
-        />
-      ) : (
-        <div className={`${css.wrapper} ${loaded ? css.show : ""}`}>
-          <Preview ref={sliderRef} gallery={gallery} loading={loading} />
-          {
-            <div className={css.thumbnail}>
-              <NavigationBtn onClick={onPrev} />
-              <Thumbnail
-                currentSlide={currentSlide}
-                ref={thumbnailRef}
-                gallery={gallery}
-                loading={loading}
-              />
-              <NavigationBtn onClick={onNext} isNext />
-            </div>
-          }
-          {gallery?.images?.length > 1 ? (
-            <Dots
-              onChangeSlide={onChangeSlide}
-              currentSlide={currentSlide}
-              items={gallery?.images?.length}
-            />
-          ) : (
-            ""
-          )}
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{loading ? (
+				<Skeleton
+					count={1}
+					className={css.skeleton}
+					containerClassName={css.skeleton_container}
+				/>
+			) : (
+				<div className={`${css.wrapper} ${loaded ? css.show : ""}`}>
+					<Preview
+						ref={sliderRef}
+						gallery={gallery}
+						loading={loading}
+					/>
+
+					<div className={css.thumbnail}>
+						{!!gallery?.images?.length && (
+							<>
+								<NavigationBtn onClick={onPrev} />
+								<Thumbnail
+									currentSlide={currentSlide}
+									ref={thumbnailRef}
+									gallery={gallery}
+									loading={loading}
+								/>
+								<NavigationBtn onClick={onNext} isNext />
+							</>
+						)}
+					</div>
+					{gallery?.images?.length > 1 ? (
+						<Dots
+							onChangeSlide={onChangeSlide}
+							currentSlide={currentSlide}
+							items={gallery?.images?.length}
+						/>
+					) : (
+						""
+					)}
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Gallery;
