ed8789a76f2ffa7d6ec508ca14e73bd74d2adeb8
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-05-25 22:01:01 +0500

    feat: sitemap & robots

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    i18n: {
-        defaultLocale: "default",
-        locales: ["uz", "ru", "default"]
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-                pathname: '**',
-            },
-        ],
-        minimumCacheTTL:1500000
-    },
-    output: "standalone"
-}
+	reactStrictMode: true,
+	i18n: {
+		defaultLocale: "default",
+		locales: ["uz", "ru", "default"],
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+				port: "",
+				pathname: "**",
+			},
+		],
+		minimumCacheTTL: 1500000,
+	},
+	output: "standalone",
+	async rewrites() {
+		return [
+			{
+				source: "/robots.txt",
+				destination: "/api/robots",
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,233 @@
+import { siteConfig } from "@/config/site";
+import { IProductsWithPagination, IStore } from "@/data-types/products/common";
+import axios from "axios";
+
+const EXTERNAL_DATA_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+function generateSiteMap({
+	products,
+	stores,
+}: {
+	products: IProductsWithPagination;
+	stores: IStore[];
+}) {
+	return `<?xml version="1.0" encoding="UTF-8"?>
+	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <!--We manually set the two URLs we know already-->
+	<url>
+	<loc>${siteConfig.url}/</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>1.00</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/about</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/stores</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/products?sort=popular&amp;onSales=true</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/products?sort=popular</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.80</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/about</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/about</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/stores</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/stores</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/products?sort=popular&amp;onSales=true</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/products?sort=popular</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/products?sort=popular&amp;onSales=true</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/products?sort=popular</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/products</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/products</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/products</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/uz/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/ru/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<url>
+	<loc>${siteConfig.url}/catalog</loc>
+	<lastmod>${new Date().toISOString()}</lastmod>
+	<priority>0.64</priority>
+	</url>
+	<!-- sitemap for products -->
+  ${products?.content
+		?.map(
+			({ id }) => `<url>
+        <loc>${siteConfig.url}/products/${id}</loc>
+       	<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>weekly</changefreq>
+				<priority>0.55</priority>
+      </url>
+			<url>
+				<loc>${siteConfig.url}/ru/products/${id}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>weekly</changefreq>
+				<priority>0.55</priority>
+			</url>
+			<url>
+				<loc>${siteConfig.url}/uz/products/${id}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>weekly</changefreq>
+				<priority>0.55</priority>
+			</url>
+    `
+		)
+		.join("")}
+		<!-- sitemap for stores -->
+		${stores
+			?.map(
+				({ id }) => `<url>
+				<loc>${siteConfig.url}/stores/${id}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>monthly</changefreq>
+				<priority>0.5</priority>
+		 </url>
+		 <url>
+				<loc>${siteConfig.url}/ru/stores/${id}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>monthly</changefreq>
+				<priority>0.5</priority>
+		 </url>
+		 <url>
+				<loc>${siteConfig.url}/uz/stores/${id}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>monthly</changefreq>
+				<priority>0.5</priority>
+		 </url>
+		`
+			)
+			.join("")}
+  </urlset>
+`;
+}
+
+function SiteMap() {
+	// getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }: any) {
+	// We make an API call to gather the URLs for our site
+	const products = await getProducts();
+	const stores = await getStores();
+
+	// We generate the XML sitemap with the posts data
+	const sitemap = generateSiteMap({
+		products,
+		stores,
+	});
+
+	res.setHeader("Content-Type", "text/xml");
+	// we send the XML to the browser
+	res.write(sitemap);
+	res.end();
+
+	return {
+		props: {},
+	};
+}
+
+const getProducts = async () => {
+	const res = await axios.get<{ result: IProductsWithPagination }>(
+		EXTERNAL_DATA_URL + "/store/products/",
+		{
+			params: { size: 10000 },
+		}
+	);
+
+	return res.data.result;
+};
+
+const getStores = async () => {
+	const res = await axios.get<{ result: IStore[] }>(
+		EXTERNAL_DATA_URL + "/store/"
+	);
+
+	return res.data.result;
+};
+
+export default SiteMap;
