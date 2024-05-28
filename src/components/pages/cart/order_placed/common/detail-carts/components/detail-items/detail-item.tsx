0497cd93bc090d21a9a1f09ac61100be5239332d
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-28 14:29:05 +0500

    add preloader in detailt cart

--- src/components/pages/cart/order_placed/common/detail-carts/components/detail-items/detail-item.tsx
+++ src/components/pages/cart/order_placed/common/detail-carts/components/detail-items/detail-item.tsx
@@ -1,26 +1,34 @@
-import css from './detail-item.module.css'
-import {ReactElement} from "react";
+import css from "./detail-item.module.css";
+import { ReactElement } from "react";
+import Skeleton from "react-loading-skeleton";
 
 interface props {
-    label: string
-    value?: string | number
-    className?: string
-    label_prefix?:ReactElement
+  label: string;
+  value?: string | number;
+  className?: string;
+  label_prefix?: ReactElement;
+  loading?: boolean;
 }
 
-const DetailItem
-    = ({label, value, className,label_prefix}: props) => {
-    return (
-        <div className={`${css.item} ${className ? className : ''}`}>
-            <h5 className={css.label}>
-                {label}{label_prefix}
-            </h5>
-            <p className={`${css.value} `}>
-                {value}
-            </p>
-        </div>
-    );
+const DetailItem = ({
+  label,
+  value,
+  className,
+  label_prefix,
+  loading,
+}: props) => {
+  return (
+    <div className={`${css.item} ${className ? className : ""}`}>
+      <h5 className={css.label}>
+        {label}
+        {label_prefix}
+      </h5>
+      <p className={`${css.value} `}>
+        {value}
+        {loading ? <Skeleton count={1} className={css.skeleton} /> : ""}
+      </p>
+    </div>
+  );
 };
 
-export default DetailItem
-;
+export default DetailItem;
