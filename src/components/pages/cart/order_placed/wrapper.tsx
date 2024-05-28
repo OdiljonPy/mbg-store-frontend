5b20924d1654e6ab9b5b36181877f5f262c974b9
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-05-28 14:22:33 +0500

    fix:order placed confict figma

--- src/components/pages/cart/order_placed/wrapper.tsx
+++ src/components/pages/cart/order_placed/wrapper.tsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import css from "./wrapper.module.css";
 import OrderCostCard from "@/components/pages/account/orders/order/order-cost-card/order-cost-card";
 import OrderError from "@/components/pages/cart/order_placed/common/order-error/OrderError";
+import { cn } from "@/utils/cn";
 interface props {}
 
 const Wrapper = (props: props) => {
@@ -27,38 +28,32 @@ const Wrapper = (props: props) => {
 
   if (err) return <OrderError />;
   return (
-    <section className={css.cart}>
-      <div className={"container"}>
-        <Breadcrumbs
-          items={[
-            {
-              path: "/",
-              label: t("header.home"),
-            },
-            {
-              path: "/cart",
-              label: t("header.basket"),
-            },
-            {
-              path: "/cart/delivery",
-              label: t("header.order_placed"),
-            },
-          ]}
-        />
-        <div className={css.status}>
-          <Status last_order={last_order} loading={loading} />
-        </div>
-        <div className={css.wrapper}>
-          <Content loading={loading} last_order={last_order} />
-          <aside className={css.detail}>
-            <DetailCart />
-            <OrderCostCard
-              setErr={setErr}
-              order={last_order}
-              loading={loading}
-            />
-          </aside>
-        </div>
+    <section className={cn("container", css.cart)}>
+      <Breadcrumbs
+        items={[
+          {
+            path: "/",
+            label: t("header.home"),
+          },
+          {
+            path: "/cart",
+            label: t("header.basket"),
+          },
+          {
+            path: "/cart/delivery",
+            label: t("header.order_placed"),
+          },
+        ]}
+      />
+      <div className={css.status}>
+        <Status last_order={last_order} loading={loading} />
+      </div>
+      <div className={css.wrapper}>
+        <Content loading={loading} last_order={last_order} />
+        <aside className={css.detail}>
+          <DetailCart />
+          <OrderCostCard setErr={setErr} order={last_order} loading={loading} />
+        </aside>
       </div>
     </section>
   );
